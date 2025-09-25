c8033a2acb90e72fcf00b3f2a110e95d94b82f90
Anubhav Shukla <kumaranubhav691@gmail.com>
2025-09-25 21:58:03 +0530

    now work on logins

--- src/context/HardCodedValues.jsx
+++ src/context/HardCodedValues.jsx
@@ -1,3 +1,3 @@
-// const backend_url = "https://hirebuddy-backend-7e8u.onrender.com";
-const backend_url = "http://localhost:7777" | "https://hirebuddy-backend-7e8u.onrender.com";
+const backend_url = "https://hirebuddy-backend-7e8u.onrender.com";
+// const backend_url = "http://localhost:7777" | "https://hirebuddy-backend-7e8u.onrender.com";
 export { backend_url }
